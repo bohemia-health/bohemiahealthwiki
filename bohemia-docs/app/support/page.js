39253690ec542a770c7c914cceef057e4c3e7ecc
Janae Dooley <janaesdooley@gmail.com>
2026-06-12 08:18:59 -0500

    support page error handling

---
+++ bohemia-docs/app/support/page.js
@@ -0,0 +1,18 @@
+export default function SupportPage() {
+  return (
+    <main className="content">
+      <div className="breadcrumb">
+        <a href="/">Docs</a>
+        <span>{">"}</span>
+        <span>Get started</span>
+        <span>{">"}</span>
+        <a href="/support">Support</a>
+      </div>
+
+      <section className="docs-page">
+        <h1>Support</h1>
+        <p>Lorem ipsum</p>
+      </section>
+    </main>
+  );
+}
